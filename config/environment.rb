5c2e0921affb10c3468c852d71f19d7da6f11dbe
Morten Rønne <morten.roenne@tdcadsl.dk>
2013-06-25 12:17:28 +0200

    Remove colorize from logs.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Haitatsu::Application.initialize!
+
+Haitatsu::Application.configure do
+  config.colorize_logging = false
+end
+
