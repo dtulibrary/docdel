adfcea6e6baeca5d6088cb0731a690a5015966e1
Morten Rønne <mron.dtic.dtu.dk>
2013-09-02 11:30:58 +0200

    Updated time zone to CET.

--- config/application.rb
+++ config/application.rb
@@ -38,13 +38,14 @@ module Haitatsu
     # auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names.
     # Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'CET'
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from
     # config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales',
     #   '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -99,7 +100,7 @@ module Haitatsu
     c.systemmail = ''
     c.order_prefix = ''
 #    c.accounts = {
-#      'group1' => { 
+#      'group1' => {
 #        'user' => '',
 #        'password' => '',
 #      },
