eb0dee247dfd414ef9893271828ea2c58cebde25
Viktor Smari <viktorsmari@gmail.com>
2016-03-18 12:41:02 +0100

    Remove debug view. We have /admin to view  orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,4 @@
 class OrdersController < ApplicationController
-  def index
-    @orders = Order.all
-  end
-
  def show
    @order = Order.find(params[:id])
    @order_request = @order.system_request('local_scan')
