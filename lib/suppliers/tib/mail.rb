cd4bd4d97bd4b1fb62ad74e53fbe5169ebcd8098
Jimmy Petersen <jipe@dtu.dk>
2016-04-15 07:18:09 +0200

    Update comment

--- lib/suppliers/tib/mail.rb
+++ lib/suppliers/tib/mail.rb
@@ -2,7 +2,7 @@ require 'incoming_mail_controller'
 
 class IncomingMailController
   def supplier_mail_check_tib(mail)
-    # Return true if this is a TIB mail
+    # Return true if this is a TIB mail and we handled it
     return false
   end
 
