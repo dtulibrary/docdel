7fc388435e870ecb2a22f406f4e0f04a07eb150c
Troels Lisberg Nielsen <troels.lisberg@gmail.com>
2016-08-03 13:34:59 +0200

    Add a TIB test

--- spec/controllers/incoming_mail_controller_spec.rb
+++ spec/controllers/incoming_mail_controller_spec.rb
@@ -302,6 +302,80 @@ describe IncomingMailController do
   end
 
   context 'TIB' do
+    it "handles a delivery where everything goes smooth" do
+      #
+      # Setup mock Rails configuration
+      #
+      Rails.application.config.order_prefix = 'TEST'
+
+      #
+      # Setup some mocks
+      #
+      FactoryGirl.create(:order_status, code: 'deliver')
+      FactoryGirl.create(:order_status, code: 'confirm')
+      ext = FactoryGirl.create(:external_system, code: 'tib')
+      @order = FactoryGirl.create(:order, id: 1234, callback_url: "http://callbackhost/callback")
+      @request = FactoryGirl.create(:order_request, :order => @order,
+        :external_system => ext, :external_number => nil)
+
+      #
+      # We will use this variable later
+      #
+      status = nil
+
+      #
+      # Stub some web requests
+      #
+      callback_lambda = lambda do |request|
+        query_values = request.uri.query_values
+
+        if not query_values["status"].eql?(status)
+          raise Exception.new("callback service was called with a bad status: #{status}")
+        end
+
+        if "deliver".eql?(status) && !"http://storeit:3000/dummy_document.pdf".eql?(query_values["url"])
+          raise Exception.new("callback service was called with a bad url: #{query_values['url']}")
+        end
+
+        if "confirm".eql?(status) && (!@request.external_number.nil? && !@request.external_number.eql?(query_values["supplier_order_id"]))
+          raise Exception.new("callback service was called with a bad supplier_order_id.")
+        end
+
+        return {status: 200, body: '', headers: {}}
+      end
+      stub_request(:get, /^http:\/\/callbackhost\/callback/).to_return(callback_lambda)
+
+      storeit_lambda = lambda do |request|
+        body_is_pdf_document = "%PDF".eql?(request.body[0..3])
+        # TODO TLNI: Should the body be a pdf document? Should the document be base64 encoded?
+        return :status => 200, :body => "http://storeit:3000/dummy_document.pdf", :headers => {}
+      end
+      stub_request(:post, "http://storeit:3000/rest/documents.text?drm=true").to_return(storeit_lambda)
+
+      #
+      # First we test when a 'confirm' mail is received
+      #
+      status = 'confirm'
+
+      mail = Mail.new(File.read("spec/fixtures/tib/accepted.eml"))
+      IncomingMailController.receive(mail)
+
+      expect(Order.find(@order.id).current_request.order_status.code).to eq 'confirm'
+      expect(Order.find(@order.id).current_request.external_number).to eq 'E012345678'
+
+      #
+      # ... Then we test when a 'deliver' mail is received
+      #
+      status = 'deliver'
+
+      mail = Mail.new(
+        File.read("spec/fixtures/tib/delivery_drm.eml"))
+      IncomingMailController.receive(mail)
+
+      expect(Order.find(@order.id).current_request.order_status.code).to eq 'deliver'
+      expect(Order.find(@order.id).current_request.external_url).to eq 'http://storeit:3000/dummy_document.pdf'
+    end
+
     context 'when order is accepted' do
       before do
         setup_tib(1234, nil)
@@ -325,6 +399,7 @@ describe IncomingMailController do
         tib_stub_and_receive('unfilled', 'cancel')
         expect(Order.find(@order.id).current_request.format_reason).to eq 'Double-order, this order is being dealt with order no'
       end
+
     end
 
     context 'when mail matches an existing order' do
